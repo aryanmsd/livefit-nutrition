2ff0d5972c573872a3967ad09592f434d79d5d02
Aryan Walia <164078948+aryanmsd@users.noreply.github.com>
2026-03-15 19:11:02 +0530

    Delete livefit-backend/test.js

--- livefit-backend/test.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect("mongodb+srv://livefit_admin:wPRKiT5XdX38jIIq@livefit-cluster.6uyk3hp.mongodb.net/livefit")
-.then(()=>console.log("MongoDB Connected"))
-.catch(err=>console.log(err));
