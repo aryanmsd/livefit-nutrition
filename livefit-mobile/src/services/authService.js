16194e9af6a277fdce23eaa861c1034cfa462c97
Aryan Walia <shikhaaryraw87@gmail.com>
2026-04-16 18:46:57 +0530

    Fixed the signup issue of mobile app

--- livefit-mobile/src/services/authService.js
+++ livefit-mobile/src/services/authService.js
@@ -1,15 +1,15 @@
 import API from "./api";
 
 const signup = async (username, email, password) => {
-  return await API.post("/auth/signup", { username, email, password });
+  return await API.post("/api/auth/signup", { username, email, password });
 };
 
 const login = async (email, password) => {
-  return await API.post("/auth/login", { email, password });
+  return await API.post("/api/auth/login", { email, password });
 };
 
 const forgotPassword = async (email) => {
-  return await API.post("/auth/forgot-password", { email });
+  return await API.post("/api/auth/forgot-password", { email });
 };
 
 export default {
